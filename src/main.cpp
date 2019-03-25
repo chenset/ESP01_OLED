b05f8ddeee343311365949f4a940a98bbaa9dd62
chenset <4199191@qq.com>
2019-03-25 14:18:17 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -62,6 +62,48 @@ String getStrValue(String data, char separator, int index);
 time_t getNtpTime();
 
 
+// weather img
+// img array
+// "晴",
+// "多云",
+// "阴",
+// "阵雨",
+// "雷阵雨",
+// "雷阵雨伴有冰雹",
+// "雨夹雪",
+// "小雨",
+// "中雨",
+// "大雨",
+// "暴雨",
+// "大暴雨",
+// "特大暴雨",
+// "阵雪",
+// "小雪",
+// "中雪",
+// "大雪",
+// "暴雪",
+// "雾",
+// "冻雨",
+// "沙尘暴",
+// "小雨-中雨",
+// "中雨-大雨",
+// "大雨-暴雨",
+// "暴雨-大暴雨",
+// "大暴雨-特大暴雨",
+// "小雪-中雪",
+// "中雪-大雪",
+// "大雪-暴雪",
+// "浮尘",
+// "扬沙",
+// "强沙尘暴晴",
+// "霾",
+const String weatherImgMapping[] = {
+        "1", "3", "5", "7", "7", "7", "7", "7", "7", "8", "8",
+        "8", "8", "\"", "\"", "\"", "#", "#", "M", "7", "F", "7",
+        "8", "8", "8", "8", "\"", "#", "#", "J", "E", "F", "E",
+};
+
+
 void setup() {
     // Serial
     Serial.begin(baud);
@@ -157,14 +199,21 @@ void loop() {
 }
 
 void OLEDDisplayCtl() {
-
     display.clear();
 
+    // weather icon
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.setFont(Meteocons_Plain_21);
+    int iconIndex = webResponseArr[7].toInt();
+    if (iconIndex > 30 || iconIndex < 0) {
+        iconIndex = 0;
+    }
+    display.drawString(0, 0, weatherImgMapping[iconIndex]);
 
     display.setTextAlignment(TEXT_ALIGN_LEFT);
     display.setFont(Roboto_14);
     // display.drawString(26, 3, (String)temperatureOnline + "-" + (String)humidityOnline + "-" + (String)pm25Online);
-    display.drawString(0, 3, webResponseArr[2]);
+    display.drawString(23, 3, webResponseArr[2]);
 
     display.setTextAlignment(TEXT_ALIGN_RIGHT);
     display.setFont(Roboto_14);
@@ -255,6 +304,7 @@ void webApi() {
         webResponseArr[4] = getStrValue(webResponseStr, '\r', 4);
         webResponseArr[5] = getStrValue(webResponseStr, '\r', 5);
         webResponseArr[6] = getStrValue(webResponseStr, '\r', 6);
+        webResponseArr[7] = getStrValue(webResponseStr, '\r', 7);
 
     } else {
         webResponseStr = "";
