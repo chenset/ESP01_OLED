f35dc00648f99115e222aef78e8d93c03653b0be
chenset <4199191@qq.com>
2019-03-26 15:23:08 +0800

    update

--- web/index.php
+++ web/index.php
@@ -37,8 +37,9 @@ echo "";
 echo "\r";
 echo "";
 echo "\r";
-echo (round(json_decode(@file_get_contents('/dev/shm/juhe_weather.json'), true)['result']['data']['realtime']['weather']['img'] ?? 0, 0));
+//echo (round(json_decode(@file_get_contents('/dev/shm/juhe_weather.json'), true)['result']['data']['realtime']['weather']['img'] ?? 0, 0));
+echo json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['skycon'] ?? 0;
 echo "\r";
 //echo date('M.d');
-echo (round(json_decode(@file_get_contents('/dev/shm/weather.json'), true)['temp'] ?? 0, 1)).'/'.(round(json_decode(@file_get_contents('/dev/shm/juhe_weather.json'), true)['result']['data']['realtime']['weather']['humidity'] ?? 0, 0));
-
+//echo (round(json_decode(@file_get_contents('/dev/shm/weather.json'), true)['temp'] ?? 0, 1)).'/'.(round(json_decode(@file_get_contents('/dev/shm/juhe_weather.json'), true)['result']['data']['realtime']['weather']['humidity'] ?? 0, 0));
+echo (round(json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['temperature'] ?? 0, 1)).'/'.(round((json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['humidity'] ?? 0)*100, 0));
