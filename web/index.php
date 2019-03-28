6dc24c284bd2615957d16bb4b71c82c1115b7543
chenset <4199191@qq.com>
2019-03-28 23:16:18 +0800

    update

--- web/index.php
+++ web/index.php
@@ -28,7 +28,7 @@ echo "1";
 echo "\r";
 echo "12345"; // request interval
 echo "\r";
-echo (round(json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['temperature'] ?? 0, 1)).'/'.(round((json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['humidity'] ?? 0)*100, 0));
+echo (round(json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['temperature'] ?? 0, 1)).' / '.(round((json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['humidity'] ?? 0)*100, 0));
 echo "\r";
 echo 'P: '.(json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['pm25'] ?? 0);
 echo "\r";
