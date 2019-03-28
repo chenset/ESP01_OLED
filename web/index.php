b08be62e997f900cf17cff8d61bf31a8b715e473
chenset <4199191@qq.com>
2019-03-28 22:57:22 +0800

    update

--- web/index.php
+++ web/index.php
@@ -30,12 +30,12 @@ echo "12345"; // request interval
 echo "\r";
 echo (round(json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['temperature'] ?? 0, 1)).'/'.(round((json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['humidity'] ?? 0)*100, 0));
 echo "\r";
-echo date('D');
+echo 'P: '.(json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['pm25'] ?? 0);
 echo "\r";
 echo round(getTemperature($json),1);
 echo "\r";
-echo "";
+echo 'V: '.(round(json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['visibility'] ?? 0));
 echo "\r";
-echo date('M.d');
+echo 'C: '.(round(json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['cloudrate'] ?? 0,2)*100);
 echo "\r";
-echo json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['skycon'] ?? 0; 
+echo json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['skycon'] ?? 0;
