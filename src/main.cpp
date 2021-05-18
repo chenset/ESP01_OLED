ae0fc8b830275a0a1eac95de7b2eab943acd2814
chen <chenliangzhi@wimift.com>
2021-05-18 17:28:15 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -297,7 +297,7 @@ void webApi() {
     int httpCode = http.GET();
 
     webApiTimeStr = (String) (millis() - start - fix);
-    if (httpCode > 0) {
+    if (httpCode == 200) {
 
         webResponseStr = (String) http.getString();
         webResponseArr[0] = getStrValue(webResponseStr, '\r', 0);
