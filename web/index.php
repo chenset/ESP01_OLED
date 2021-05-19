48d2ec83715b22092cd2ab02b68823a86bfb75ee
chenset <4199191@qq.com>
2021-05-19 14:31:15 +0800

    update

--- web/index.php
+++ web/index.php
@@ -11,7 +11,8 @@ if($_GET['off'] != 1){
 echo "\r";
 echo "12345"; // request interval
 echo "\r";
-echo (round(json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['temperature'] ?? 0, 1)).'/'.(round((json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['humidity'] ?? 0)*100, 0));
+#echo (round(json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['temperature'] ?? 0, 1)).'/'.(round((json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['humidity'] ?? 0)*100, 0));
+echo ' '.(round(json_decode(@file_get_contents('/dev/shm/eth.json'), true)['last'] ?? 0, 1));
 echo "\r";
 //echo date('D');
 //$exchange = json_decode(@file_get_contents('/dev/shm/exchangeratesapi.json'), true)['rates']['USD'] ?? 0; 
