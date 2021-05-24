1a5bd0ec5623b4f2eeee0d750d8203cf416eeb69
chenset <4199191@qq.com>
2021-05-24 22:10:20 +0800

    update

--- web/index.php
+++ web/index.php
@@ -47,3 +47,6 @@ echo json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), tr
 echo "\r";
 //echo (round(json_decode(@file_get_contents('/dev/shm/weather.json'), true)['temp'] ?? 0, 1)).'/'.(round(json_decode(@file_get_contents('/dev/shm/juhe_weather.json'), true)['result']['data']['realtime']['weather']['humidity'] ?? 0, 0));
 echo (round(json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['temperature'] ?? 0, 1)).'/'.(round((json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['humidity'] ?? 0)*100, 0));
+echo "\r";
+echo time();
+
