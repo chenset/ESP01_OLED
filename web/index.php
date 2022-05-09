4d483ecbe268ab5b58851e2da5b3861315cb804e
chenset <4199191@qq.com>
2022-05-09 10:54:01 +0800

    update

--- web/index.php
+++ web/index.php
@@ -11,7 +11,9 @@ if($_GET['off'] != 1){
 echo "\r";
 echo "12345"; // request interval
 echo "\r";
-echo (round(json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['temperature'] ?? 0, 1)).'/'.(round((json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['humidity'] ?? 0)*100, 0));
+#echo (round(json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['temperature'] ?? 0, 1)).'/'.(round((json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['humidity'] ?? 0)*100, 0));
+$sina = explode(',', trim(@file_get_contents('/dev/shm/sina_stock.js')??''));
+echo round($sina[6] ?? 0, 3);
 echo "\r";
 #$exchange = json_decode(@file_get_contents('/dev/shm/juhe_exchangerates.json'), true)['result'][0]['data1']['bankConversionPri'] ?? 0; 
 #echo round(10000/$exchange,3);
@@ -29,8 +31,9 @@ echo round($sina[3] ?? 0, 2).'%';
 echo "\r";
 #$exchange = json_decode(@file_get_contents('/dev/shm/juhe_exchangerates.json'), true)['result'][0]['data4']['bankConversionPri'] ?? 0; 
 #echo round(100/$exchange,3);
-$sina = explode(',', trim(@file_get_contents('/dev/shm/sina_stock.js')??''));
-echo round($sina[6] ?? 0, 3);
+#$sina = explode(',', trim(@file_get_contents('/dev/shm/sina_stock.js')??''));
+#echo round($sina[6] ?? 0, 3);
+echo (round(json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['temperature'] ?? 0, 1)).'/'.(round((json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['humidity'] ?? 0)*100, 0));
 echo "\r";
 echo json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['skycon'] ?? 0; 
 echo "\r";
