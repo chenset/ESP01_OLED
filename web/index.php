d487fa62aec402a750be3aa7867d835835e6996f
chenset <4199191@qq.com>
2022-05-09 21:17:39 +0800

    update

--- web/index.php
+++ web/index.php
@@ -28,7 +28,7 @@ function getHumidity($json){
 
 if($_GET['off'] != 1){ 
         echo "1";   // display enable
-}elseif(date('Gi') > 855 && date('G') < 21){
+}elseif(date('Gi') > 855 && date('G') < 22){
         echo "1";   // display enable
 }else{
         echo "0";   // display enable
