59bc2aeba7993c8a944500b2362a0c72419cf966
chenset <4199191@qq.com>
2022-05-31 18:02:15 +0800

    update

--- web/index.php
+++ web/index.php
@@ -41,7 +41,7 @@ echo "\r";
 #$sina = explode(',', trim(@file_get_contents('/dev/shm/sina_stock.js')??''));
 #echo round($sina[6] ?? 0, 3);
 #echo (round(json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['temperature'] ?? 0, 1)).'/'.(round((json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['humidity'] ?? 0)*100, 0));
-echo empty($sina[6]) ? "-": round($SH50ETF_earn,3);
+echo empty($sina[6]) ? "-": round($SH50ETF_earn,1);
 echo "\r";
 echo json_decode(@file_get_contents('/dev/shm/caiyun_realtime_weather.json'), true)['result']['skycon'] ?? 0; 
 echo "\r";
